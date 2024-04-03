baa132cda72880934d14cab994f0ea7e1c3500d3
Renan <renandfochetto@gmail.com>
2024-04-03 20:15:41 -0300

    atualização aula 2

---
+++ app.js
@@ -0,0 +1,18 @@
+let numeroSecreto = gerarNumeroAleatorio();
+
+function exibirTextoNaTela (tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
+}
